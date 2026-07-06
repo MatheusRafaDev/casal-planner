f422ca275d78de63dae46be2be0ff6ea1b666eae
Matheus Rafael <90002960+MatheusRafaDev@users.noreply.github.com>
2026-07-06 12:38:34 -0300

    refatoração: apagando arquivos não mais necessarios

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { PublicHeader } from "@/components/home/PublicHeader";
-import { HeroSection } from "@/components/home/HeroSection";
-import { FeaturesSection } from "@/components/home/FeaturesSection";
-import { CtaSection } from "@/components/home/CtaSection";
+import { PublicHeader } from "@/components/Home/PublicHeader";
+import { HeroSection } from "@/components/Home/HeroSection";
+import { FeaturesSection } from "@/components/Home/FeaturesSection";
+import { CtaSection } from "@/components/Home/CtaSection";
 
 export const Route = createFileRoute("/")({
   component: Index,
