d165553f691555a91eef477fff9ec46ab27ea6d1
Matheus Rafael <90002960+MatheusRafaDev@users.noreply.github.com>
2026-09-17 16:27:28 -0300

    revert: restaurar componentes de adicionar item para o commit cb3f9239

--- frontend/src/components/planejamento/ItemFormModal.tsx
+++ frontend/src/components/planejamento/ItemFormModal.tsx
@@ -416,7 +416,6 @@ export function ItemFormModal({
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="comprado">Será comprado</SelectItem>
-                  <SelectItem value="desejo">Quero ganhar (Lista Pública)</SelectItem>
                   <SelectItem value="ganho">Ganho / Presente</SelectItem>
                   {form.origem === "prometido" && (
                     <SelectItem value="prometido">Prometido por convidado</SelectItem>
